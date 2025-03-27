31cf70c45b073508c0b6859f37963fbcae0937a1
Rianne Magtuba <99489243+Rianne-Magtuba@users.noreply.github.com>
2025-03-27 12:44:29 +0800

    seperated business and ui logic

---
+++ BusinessLogic/inventoryProcess.cs
@@ -0,0 +1,54 @@
+﻿namespace BusinessLogic
+{
+    public class inventoryProcess
+    {
+
+        public static List<String> productName = new List<String>();
+        public static List<int> productQty = new List<int>();
+        public static List<Double> productPrice = new List<Double>();
+
+        public static bool checkStocksAmount()
+        {
+
+            return productName.Count==0;
+        }
+
+        public static void addItemToList(String ProductName, int ProductQty, Double ProductPrice)
+        {
+            productName.Add(ProductName);
+            productQty.Add(ProductQty);
+            productPrice.Add(ProductPrice);
+          
+        }
+
+        public static void updateAllAtProdId(String newProductName, int newProductQty, Double newProductPrice, int id)
+        {
+            productName[id] = newProductName;
+            productQty[id] = newProductQty;
+            productPrice[id] = newProductPrice;
+        }
+
+        public static void updateProductNameAtProdId(String newProductName, int id)
+        {
+            productName[id] = newProductName;
+        }
+
+        public static void updateProductQtyAtProdId(int newProductQty, int id)
+        {
+            productQty[id] = newProductQty;
+        }
+        public static void updateProductPriceAtProdId(Double newProductPrice, int id)
+        {
+            productPrice[id] = newProductPrice;
+        }
+
+        public static void removeProduct(int id)
+        {
+            inventoryProcess.productName.RemoveAt(id - 1);
+            inventoryProcess.productQty.RemoveAt(id - 1);
+            inventoryProcess.productPrice.RemoveAt(id - 1);
+
+        }
+
+    }
+}
--- inventoryManagement/Program.cs
+++ inventoryManagement/Program.cs
@@ -1,29 +1,26 @@
 ﻿using System;
+using BusinessLogic;
 
 namespace inventoryManagement
 {
     internal class Program
     {
-         List<String> productName;
-        List<int> productQty;
-        List<Double> productPrice;
+        
         static void Main(string[] args)
         {
             Console.WriteLine("Computer Inventory Management");
-            
-            Program program = new Program();
+
+
             //list initialization
-            program.productName = new List<String>();
-            program.productQty = new List<int>();
-            program.productPrice = new List<Double>();
 
             //testing vlaues
-            program.productName.Add("test");
-            program.productName.Add("tes2t");
-            program.productQty.Add(5);
-            program.productQty.Add(3);
-            program.productPrice.Add(67);
-            program.productPrice.Add(44);
+            Console.WriteLine("TEST");
+            inventoryProcess.productName.Add("test");
+            inventoryProcess.productName.Add("tes2t");
+            inventoryProcess.productQty.Add(5);
+            inventoryProcess.productQty.Add(3);
+            inventoryProcess.productPrice.Add(67);
+            inventoryProcess.productPrice.Add(44);
 
             Boolean retry = false;
             do
@@ -38,24 +35,24 @@ namespace inventoryManagement
                 switch (choice)
                 {
                     case 1:
-                        program.viewStocks();
-                        retry = program.tryAgain();
+                        viewStocks();
+                        retry = tryAgain();
                         break;
 
                     case 2:
-                        program.addItem();
-                        retry = program.tryAgain();
+                        addItem();
+                        retry = tryAgain();
                         break;
 
                     case 3:
-                        program.updateStocks();
-                        retry = program.tryAgain();
+                        updateStocks();
+                        retry = tryAgain();
                         break;
 
                     case 4:
 
-                        program.removeItem();
-                        retry = program.tryAgain();
+                      removeItem();
+                        retry =tryAgain();
                         break;
 
                     default:
@@ -66,7 +63,7 @@ namespace inventoryManagement
             
         }
 
-        public Boolean tryAgain()
+        static public Boolean tryAgain()
         {
             Boolean tryAgain = false;
             Console.WriteLine("Do you want to run the program again?  Y/N");
@@ -89,11 +86,12 @@ namespace inventoryManagement
 
                 return tryAgain;
         }
-        public void addItem()
+
+        static public void addItem()
         {
             String pName;
-            int pQty;
-            double pPrice;
+            int pQty=0;
+            double pPrice=0;
 
             Console.WriteLine("Please enter a product name");
             pName = Console.ReadLine();
@@ -105,6 +103,7 @@ namespace inventoryManagement
                 Console.WriteLine("Please enter its Price");
                 pPrice = Convert.ToDouble(Console.ReadLine());
 
+
             }
             catch (FormatException e)
             {
@@ -112,102 +111,123 @@ namespace inventoryManagement
                 Console.WriteLine("ERROR! Please enter a number.");
             }
 
-         
-        }
-        
-       
-        public void viewStocks()
-        {
-            Console.WriteLine("Product Name\t| Qty \t| Price ");
-            String space = "  ";
-            for (int i =0; i < productName.Count(); i++)
-            { 
-                Console.WriteLine("  " + productName[i].Trim() + "        \t|    " + Convert.ToString(productQty[i]) + "\t|  "+ Convert.ToString(productPrice[i]));
+            inventoryProcess.addItemToList(pName,pQty,pPrice);
 
+
+
+        }
+
+
+        static public void viewStocks()
+        {
+     
+            if (inventoryProcess.checkStocksAmount() == true)
+            {
+                Console.WriteLine("Your Stocks is empty please add stocks");
+            }  else   {
+                Console.WriteLine("Product Name\t  Qty \t Price \t");
+                for (int i = 0; i < inventoryProcess.productName.Count(); i++)
+                {
+                    Console.WriteLine("  " + inventoryProcess.productName[i] + " [" + (i + 1) + "]" + " \t  " + Convert.ToString(inventoryProcess.productQty[i]) + "\t " + Convert.ToString(inventoryProcess.productPrice[i]));
+
+                }
             }
+
+         
            
         }
 
-        public void updateStocks()
+        static public void updateStocks()
         {
-            Console.WriteLine("Product Name\t  Qty \t Price \t");
-            for (int i = 0; i < productName.Count(); i++)
+            if(inventoryProcess.checkStocksAmount() == true)
             {
-                Console.WriteLine("  " + productName[i] + " [" + (i + 1) + "]" + " \t  " + Convert.ToString(productQty[i]) + "\t " + Convert.ToString(productPrice[i]));
-
-            }
-            Console.WriteLine("Please select  the product id  that you want to update: ");
-            int input = Convert.ToInt16(Console.ReadLine());
-
-            Console.WriteLine("You've selected " + productName[input-1] + " What values do you want to change? ");
-            Console.WriteLine("productName[1]\t product quantity[2] \t product price[3] \t  all of them[4]");
-            int choice = Convert.ToInt16(Console.ReadLine());
-            switch (choice)
-            {
-                case 1:
-                    Console.WriteLine("Enter new value for product Name");
-                    productName[input - 1] = Console.ReadLine().Trim();
-
-                    break;
-
-                case 2:
-                    Console.WriteLine("Enter new value for product Quantity");
-                    productQty[input - 1] = Convert.ToInt32(Console.ReadLine().Trim());
-                    break;
-
-                case 3:
-                    Console.WriteLine("Enter new value for product price");
-                    productPrice[input - 1] = Convert.ToDouble(Console.ReadLine().Trim());
-
-                    break;
-
-               case 4:
-                    Console.WriteLine("Enter new value for product Name");
-                    productName[input - 1] = Console.ReadLine().Trim();
-                    Console.WriteLine("Enter new value for product Quantity");
-                    productQty[input - 1] = Convert.ToInt32(Console.ReadLine().Trim());
-                    Console.WriteLine("Enter new value for product price");
-                    productPrice[input - 1] = Convert.ToDouble(Console.ReadLine().Trim());
-               
-                    break;
-                default:
-                    Console.WriteLine("Error Please enter the required input");
-
-                    break;
-
-            }
-
-
-        }
-        public void removeItem()
-        {
-         
-            Console.WriteLine("Product Name\t  Qty \t Price \t");
-            for (int i = 0; i < productName.Count(); i++)
-            {
-                Console.WriteLine("  " + productName[i] + " ["+ (i+1) + "]" + " \t  " + Convert.ToString(productQty[i]) + "\t " + Convert.ToString(productPrice[i]));
-
-            }
-            Console.WriteLine("Please select  the product id  that you want to remove: ");
-            int input = Convert.ToInt16(Console.ReadLine());
-
-            Console.WriteLine("Are you sure you want to remove the product: " + productName[input-1] + "? Y/N");
-            String ans = Console.ReadLine();
-
-            if (ans.ToUpper().Equals("Y"))
-            {
-                productName.RemoveAt(input - 1);
-                productQty.RemoveAt(input - 1);
-                productPrice.RemoveAt(input - 1);
-                Console.WriteLine("Item removed succesfully");
-            }
-            else if (ans.ToUpper().Equals("N"))
-            {
-                Console.WriteLine("Action cancelled");
+                Console.WriteLine("Your Stocks is empty please add stocks");
             }
             else
             {
-                Console.WriteLine("ERROR! Please input the requested value");
+                viewStocks();
+
+                Console.WriteLine("Please select  the product id  that you want to update: ");
+                int input = Convert.ToInt16(Console.ReadLine());
+
+                Console.WriteLine("You've selected " + inventoryProcess.productName[input - 1] + " What values do you want to change? ");
+                Console.WriteLine("productName[1]\t product quantity[2] \t product price[3] \t  all of them[4]");
+                int choice = Convert.ToInt16(Console.ReadLine());
+                String newProductName;
+                int newProductQty = 0;
+                Double newProductPrice = 0;
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Enter new value for product Name");
+                        newProductName = Console.ReadLine().Trim();
+                        inventoryProcess.updateProductNameAtProdId(newProductName, input - 1);
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Enter new value for product Quantity");
+                        newProductQty = Convert.ToInt32(Console.ReadLine().Trim());
+                        inventoryProcess.updateProductQtyAtProdId(newProductQty, input - 1);
+
+                        break;
+
+                    case 3:
+                        Console.WriteLine("Enter new value for product price");
+                        newProductPrice = Convert.ToDouble(Console.ReadLine().Trim());
+                        inventoryProcess.updateProductPriceAtProdId(newProductPrice, input - 1);
+                        break;
+
+                    case 4:
+                        Console.WriteLine("Enter new value for product Name");
+                        newProductName = Console.ReadLine().Trim();
+                        Console.WriteLine("Enter new value for product Quantity");
+                        newProductQty = Convert.ToInt32(Console.ReadLine().Trim());
+                        Console.WriteLine("Enter new value for product price");
+                        newProductPrice = Convert.ToDouble(Console.ReadLine().Trim());
+
+
+                        inventoryProcess.updateAllAtProdId(newProductName, newProductQty, newProductPrice, input - 1);
+                        break;
+                    default:
+                        Console.WriteLine("Error Please enter the required input");
+
+                        break;
+
+                }
+            }
+               
+
+
+        }
+        static public void removeItem()
+        {
+            if (inventoryProcess.checkStocksAmount())
+            {
+                Console.WriteLine("Your Stocks is empty please add stocks");
+            }
+            else {
+                viewStocks();
+
+                Console.WriteLine("Please select  the product id  that you want to remove: ");
+                int input = Convert.ToInt16(Console.ReadLine());
+
+                Console.WriteLine("Are you sure you want to remove the product: " + inventoryProcess.productName[input - 1] + "? Y/N");
+                String ans = Console.ReadLine();
+
+                if (ans.ToUpper().Equals("Y"))
+                {
+                    inventoryProcess.removeProduct(input - 1);
+                    Console.WriteLine("Item removed succesfully");
+                }
+                else if (ans.ToUpper().Equals("N"))
+                {
+                    Console.WriteLine("Action cancelled");
+                }
+                else
+                {
+                    Console.WriteLine("ERROR! Please input the requested value");
+                }
+
             }
 
         }
