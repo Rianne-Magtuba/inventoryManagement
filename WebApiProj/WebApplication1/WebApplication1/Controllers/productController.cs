55c98964f7e35feb7d4d60304f76a7a534a3dc7f
Rianne Magtuba <99489243+Rianne-Magtuba@users.noreply.github.com>
2025-06-24 00:08:13 +0800

    added webApi controllers

---
+++ WebApiProj/WebApplication1/WebApplication1/Controllers/productController.cs
@@ -0,0 +1,49 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using BusinessLogic;
+using InventoryCommon;
+using DataLogic;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class productController : ControllerBase
+    {
+        inventoryProcess inventoryProcess = new inventoryProcess();
+        [HttpGet]
+        public IEnumerable<Product> get()
+        {
+            var products =  inventoryProcess.getProducts();
+            return products;
+        }
+
+        [HttpPut]
+        public void  addProduct(Product product)
+        {
+            inventoryProcess.addProduct(product.Id,product.Name,product.Quantity,product.supplierId,  product.Price, product.category);
+          
+        }
+        [HttpPatch]
+        public void updateProduct(productModule request)
+
+        {
+            string id = request.id;
+            string name = request.name;
+            int quantity = request.quantity;
+            double price = request.price;
+            string supplier = request.supplier;
+            string selectedId = request.selectedId;
+            string category = request.category;
+            inventoryProcess.updateProduct(selectedId, name, id, quantity, price, supplier,category);
+            //  inventoryProcess.updateAllProducts(request.selectedId, request.name, request.id, request.quantity, request.price, request.supplier);
+
+
+        }
+        [HttpDelete]
+        public void deleteProduct(string id)
+        {
+            inventoryProcess.removeProduct(id);
+        }
+    }
+}
---
+++ WebApiProj/WebApplication1/WebApplication1/productModule.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApplication1
+{
+    public class productModule
+    {
+        public string id { get; set; }
+
+        public string name { get; set; }
+        public  int quantity { get; set; }
+        public double price { get; set; }
+        public string supplier { get; set; }
+        public string category { get; set; }
+
+        public string selectedId { get; set; }
+    }
+}
